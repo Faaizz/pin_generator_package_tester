33244adc75daef964c3c72227c538321e2f22970
Faaizz <fr33ziey@gmail.com>
2022-05-10 02:02:01 +0200

    Update method name

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $pin = Generator::getPin();
+    $pin = Generator::generatePin();
     return view('welcome', ['pin' => $pin]);
 });
